1f840cabb41e317250ffa97c91dec1bec10d82a1
onurbural <vwpassat43@outlook.com>
2024-07-23 14:40:16 +0300

    1

---
+++ Projem1/Projem1/Migrations/20240723113024_test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projem1.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tableonur",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    İsim = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BaslangicZaman = table.Column<DateOnly>(type: "date", nullable: false),
+                    KayitZamani = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tableonur", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tableonur");
+        }
+    }
+}
---
+++ Projem1/Projem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Projem1.Models;
+
+class Program
+{
+    static void Main()
+    {
+        var context = new onurContext();
+
+        var query = context.Tableonurs.Where(x=>x.Id<3).Select(x => new
+        {
+            AradakiFark = (x.BaslangicZaman.ToDateTime(TimeOnly.MinValue) - x.KayitZamani).TotalDays
+        });
+
+        var sqlQuery = query.ToQueryString();
+
+        foreach (var result in query)
+        {
+            Console.WriteLine(result.AradakiFark);
+        }
+    }
+}
